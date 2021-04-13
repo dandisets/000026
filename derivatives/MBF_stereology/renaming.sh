f9efaa190ec6461ea46f693643e90c17a0670e82
DANDI Meta-user <dandi@mit.edu>
2021-04-13 17:16:38 -0400

    Ran backups2datalad.py
    85 files added
    13 files deleted

---
+++ derivatives/MBF_stereology/renaming.sh
@@ -0,0 +1,38 @@
+mv "Broca Slide 8_I561_f600_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS08_stain-Calretinin_STER.xlsx
+mv "Broca Slide 8_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS08_stain-NeuN_STER.xlsx
+mv "Broca Slide 9_I561_f600_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS09_stain-Calretinin_STER.xlsx
+mv "Broca Slide 9_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS09_stain-NeuN_STER.xlsx
+mv "Broca Slide 10_I561_f600_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS10_stain-Calretinin_STER.xlsx
+mv "Broca Slide 10_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS10_stain-NeuN_STER.xlsx
+mv "Broca Slide 11_I561_f600_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS11_stain-Calretinin_STER.xlsx
+mv "Broca Slide 11_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS11_stain-NeuN_STER.xlsx
+mv "Broca Slide 12_I561_f600_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS12_stain-Calretinin_STER.xlsx
+mv "Broca Slide 12_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS12_stain-NeuN_STER.xlsx
+mv "Broca Slide 13_I488_f525_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS13_stain-NeuN_STER.xlsx
+mv "Broca Slide 13_I638_f690_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS13_stain-Calretinin_STER.xlsx
+mv "Broca Slide 14_I488_f525_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS14_stain-NeuN_STER.xlsx
+mv "Broca Slide 14_I638_f690_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS14_stain-Calretinin_STER.xlsx
+mv "Broca Slide 15_I488_f525_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS15_stain-NeuN_STER.xlsx
+mv "Broca Slide 15_I638_f690_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS15_stain-Calretinin_STER.xlsx
+mv "Broca Slide 16_I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS16_stain-Calretinin_STER.xlsx
+mv "Broca Slide 16_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS16_stain-NeuN_STER.xlsx
+mv "Broca Slide 17_I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS17_stain-Calretinin_STER.xlsx
+mv "Broca Slide 17_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS17_stain-NeuN_STER.xlsx
+mv "Broca Slide 18_I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS18_stain-Calretinin_STER.xlsx
+mv "Broca Slide 18_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS18_stain-NeuN_STER.xlsx
+mv "Broca Slide 19_I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS19_stain-Calretinin_STER.xlsx
+mv "Broca Slide 19_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS19_stain-NeuN_STER.xlsx
+mv "Broca Slide 20 I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS20_stain-Calretinin_STER.xlsx
+mv "Broca Slide 20_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS20_stain-NeuN_STER.xlsx
+mv "Broca Slide 21_I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS21_stain-Calretinin_STER.xlsx
+mv "Broca Slide 21_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS21_stain-NeuN_STER.xlsx
+mv "Broca Slide 22_I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS22_stain-Calretinin_STER.xlsx
+mv "Broca Slide 22_I638_f690_NeuN_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS22_stain-NeuN_STER.xlsx
+mv "Broca Slide 23_I488_f525_Calretinin_Florence_Jessie.xlsx" sub-I46_sample-BrocaAreaS23_stain-Calretinin_STER.xlsx
+#did not rename these:
+#seem to be missing NeuN stain for slice 23
+#mv Block_I488_f525_Calretinin_Florence_Jessie.xlsx 
+#mv Block_I638_f690_NeuN_Florence_Jessie.xlsx
+#mv Broca Slide 18 Layer III_I638_f690_NeuN_Florence_Jessie.xlsx
+#mv Broca Slide 18 Layer V_I638_f690_NeuN_Florence_Jessie.xlsx
+
